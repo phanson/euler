cee7995c5669616d51919d8e9db94144448f4c46
Philip Hanson <phanson@bayasoft.com>
2012-08-31 16:53:26 -0400

    Improved Python problem 10 performance

--- python/010.py
+++ python/010.py
@@ -1,46 +1,33 @@
 # Euler problem 10
 
-# the following is reused from problem 7
-
-def isPrime(n, knownPrimes):
-    for p in reversed(knownPrimes):
-        if n % p == 0:
-            return False
-    return True
-
-def nextPrime(knownPrimes):
-    x = knownPrimes[-1] + 1
-    while not isPrime(x, knownPrimes):
-        x += 1
+# naive sieve algoritm
+def sieve(max_n):
+    x = [n for n in range(2,max_n+1)]
+    i = 0
+    while i < len(x):
+        j = i + 1
+        while j < len(x):
+            if x[j] % x[i] == 0:
+                del x[j]
+            else:
+                j += 1
+        i += 1
     return x
 
-# new functions are below:
-	
-def primeBelow(n):
-    knownPrimes = [2,3,5]
-    if n <= knownPrimes[-1]:
-        x = 1
-        for p in knownPrimes:
-            if p <= n:
-                x = p
-        return x
-    while knownPrimes[-1] <= n:
-        knownPrimes.append(nextPrime(knownPrimes))
-    return knownPrimes[-2]
-
-def sumPrimesBelow(n):
-    knownPrimes = [2,3,5]
-    while knownPrimes[-1] <= n:
-        knownPrimes.append(nextPrime(knownPrimes))
-    t = 0
-    for p in knownPrimes:
-        if p > n:
-            break
-        t += p
-    return t
-
-# note: slooooooooooooooooooooooooooooooooooooooooow.
-# future: use a sieve algorithm
+# optimized sieve algorithm
+def sieve2(max_n):
+    x = [n for n in range(2,max_n+1)]
+    i = 0
+    while i < len(x):
+        if x[i] == 0:
+            i += 1
+            continue
+        j = i + x[i]
+        while j < len(x):
+            x[j] = 0
+            j += x[i]
+        i += 1
+    return x
 
 def euler10():
-	return sumPrimesBelow(2000000)
+    return sum(sieve2(2000000))
